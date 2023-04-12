3e9111cd872f65072ed02a4677c39dee9f213893
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-13 01:04:25 +0300

    [Chore]: Add task one

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
+"""
+Square class module
+Usage: ./square.py
+"""
+
+
 class square():
-    
+    """
+    The Square class
+    """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -15,10 +22,13 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String reresentation of the square"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
